f688540360d6ea65033abbb4563f8d30e5b3b28e
Lucy <luleco63@gmail.com>
2023-04-07 11:37:23 -0400

    first commit

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundaMentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundaMentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundaMentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi/springboot/fundamentos/FundaMentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundaMentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
